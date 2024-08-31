7e80668cb60064a69fe5076845160b07232f4388
tsc <mz260959@gmail.com>
2024-08-31 10:13:08 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Image from 'next/image'
 const page = () => {
   return (
     <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
